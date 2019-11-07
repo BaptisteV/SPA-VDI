8602f51a1188904ba0418674de8fa0d936f88638
unknown <baptiste.viloin.iris@gmail.com>
2019-11-07 15:42:18 +0000

    Cleaning up dependencies and added the mandatory RickRoll

--- src/Specialites.js
+++ src/Specialites.js
@@ -1,36 +1,42 @@
 import React from "react";
 import PageTopImage from "./parts/PageTopImage";
 import certificateIcon from "./images/certificate2.png";
+import rickRoll from "./rick-roll.mp3";
 
 class Specialite extends React.Component {
 	constructor() {
-        super();
-        this.endpoint = "https://api.giphy.com/v1/gifs/random?tag=bike+stunt&api_key=lFu1aiCbnpR2Q3Wcp3baBf7fpO80K9fQ"
-        this.state = {gif: null};
+		super();
+		this.endpoint = "https://api.giphy.com/v1/gifs/random?tag=bike+stunt&api_key=lFu1aiCbnpR2Q3Wcp3baBf7fpO80K9fQ";
+		this.state = { gif: null };
 
-        this.getNewGif = this.getNewGif.bind(this);
-    }
-    
-    async componentDidMount(){
-        await this.getNewGif()
-    }
+		this.getNewGif = this.getNewGif.bind(this);
+	}
 
-    async getNewGif() {
-        const response = await fetch(this.endpoint);
-        const body = await response.json();
-        const gif = body.data.image_url;
-        this.setState({gif});
-    }
+	async componentDidMount() {
+		await this.getNewGif();
+	}
+
+	async getNewGif() {
+		const response = await fetch(this.endpoint);
+		const body = await response.json();
+		const gif = body.data.image_url;
+		this.setState({ gif });
+	}
 
 	render() {
 		return (
 			<div className="container quick-fadein">
 				<PageTopImage src={certificateIcon} alt="Specialite" />
-                <div className="text-center">
-                    <img src={this.state.gif} onClick={this.getNewGif} alt="" style={{cursor: "pointer"}}/>
-                </div>  
+				<div className="text-center">
+					<img src={this.state.gif} onClick={this.getNewGif} alt="" style={{ cursor: "pointer" }} />
+					<figure>
+						<audio controls loop src={rickRoll}>
+							Your browser does not support the
+							<code>audio</code> element.
+						</audio>
+					</figure>
+				</div>
 			</div>
-            
 		);
 	}
 }
